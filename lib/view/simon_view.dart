b1aaea1af84ea43c9ee353e789b97790613f6f7b
Eliseo Condori Mamani <eeliezercm2@gmail.com>
2025-12-01 09:14:42 -0400

    Fix pantalla desbordada

--- lib/view/simon_view.dart
+++ lib/view/simon_view.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simondice_proy/view/switch.dart';
@@ -47,95 +45,97 @@ class SimonView extends StatelessWidget {
       backgroundColor: mostrarColorDeFondo(modoOscuro),
       // appBar: AppBar(title: const Text("Simón Dice")),
       // PRINCIPAL
-      body: Column(
-
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-
-          const SizedBox(height:50),
-          Text(
-            mostrarTitulo(context),
-            style: TextStyle(color: mostrarColorDeTextoPorElModo(modoOscuro), fontSize: 50,fontFamily: "Arial",fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-
-
-          // --- PARTE B: El Tablero de 4 Botones ---
-          // En lugar de una cuadrícula automática, usamos Filas (Rows) manuales.
-
-          // FILA DE ARRIBA (Verde y Rojo)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Botón 0: Verde
-              _botonDeColor(context, 1, Colors.green),
-              const SizedBox(width: 10),
-              // Botón 1: Rojo
-              _botonDeColor(context, 2, Colors.red),
-            ],
-          ),
-          const SizedBox(height: 10),
-          // FILA DE ABAJO (Amarillo y Azul)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Botón 2: Azul (Nota: ajusté el orden para que cuadre visualmente)
-              _botonDeColor(context, 3, Colors.blue),
-              const SizedBox(width: 10),
-              // Botón 3: Amarillo
-              _botonDeColor(context, 4, Colors.yellow),
-
-            ],
-          ),
-          const SizedBox(height: 50),
-
-          // --- PARTE C: Botón de Jugar ---
-          // Solo se muestra si NO estamos jugando actualmente
-
-          if (controlador.estado == estados.espera || controlador.estado == estados.terminado) ...[
-            ElevatedButton(
-              onPressed: () {
-                // Le ordenamos al controller que inicie
-                controlador.iniciarJuego();
-              },
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("INICIAR JUEGO",style: TextStyle(color: Colors.white),),
+      body: SingleChildScrollView(
+        child: Column(
+        
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        
+            const SizedBox(height:50),
+            Text(
+              mostrarTitulo(context),
+              style: TextStyle(color: mostrarColorDeTextoPorElModo(modoOscuro), fontSize: 50,fontFamily: "Arial",fontWeight: FontWeight.bold),
             ),
-
-            const SizedBox(height: 30),
-            ElevatedButton.icon(
-              icon: const Icon(Icons.info_outline,color: Colors.white,),
-              label: const Text("Sobre manadaCorps", style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                Navigator.pushNamed(context, '/devs');
-              },
+            const SizedBox(height: 20),
+        
+        
+            // --- PARTE B: El Tablero de 4 Botones ---
+            // En lugar de una cuadrícula automática, usamos Filas (Rows) manuales.
+        
+            // FILA DE ARRIBA (Verde y Rojo)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Botón 0: Verde
+                _botonDeColor(context, 1, Colors.green),
+                const SizedBox(width: 10),
+                // Botón 1: Rojo
+                _botonDeColor(context, 2, Colors.red),
+              ],
             ),
-            const SizedBox(height: 40),
-              Center(
-                child: Column(
-                  children:[
-                    Text("Modo oscuro", style: TextStyle(color:mostrarColorDeTextoPorElModo(modoOscuro))),
-                    Switch(
-                      value: palanca.modoOscuro,
-                      onChanged: (value) {
-                        palanca.palanca(value);
-                      },
-                    ),
-                    Text("ManadaCorps © 2024 ",
-                      style: TextStyle(color: Colors.grey[600], fontSize: 14),
-                    ),
-                    Text("Todos los derechos reservados.",
-                      style: TextStyle(color: Colors.grey[600], fontSize: 14),),
-
-                  ]
-                )
-                ,
+            const SizedBox(height: 10),
+            // FILA DE ABAJO (Amarillo y Azul)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Botón 2: Azul (Nota: ajusté el orden para que cuadre visualmente)
+                _botonDeColor(context, 3, Colors.blue),
+                const SizedBox(width: 10),
+                // Botón 3: Amarillo
+                _botonDeColor(context, 4, Colors.yellow),
+        
+              ],
+            ),
+            const SizedBox(height: 50),
+        
+            // --- PARTE C: Botón de Jugar ---
+            // Solo se muestra si NO estamos jugando actualmente
+        
+            if (controlador.estado == estados.espera || controlador.estado == estados.terminado) ...[
+              ElevatedButton(
+                onPressed: () {
+                  // Le ordenamos al controller que inicie
+                  controlador.iniciarJuego();
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                ),
+                child: const Text("INICIAR JUEGO",style: TextStyle(color: Colors.white),),
               ),
-
+        
+              const SizedBox(height: 30),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.info_outline,color: Colors.white,),
+                label: const Text("Sobre manadaCorps", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/devs');
+                },
+              ),
+              const SizedBox(height: 40),
+                Center(
+                  child: Column(
+                    children:[
+                      Text("Modo oscuro", style: TextStyle(color:mostrarColorDeTextoPorElModo(modoOscuro))),
+                      Switch(
+                        value: palanca.modoOscuro,
+                        onChanged: (value) {
+                          palanca.palanca(value);
+                        },
+                      ),
+                      Text("ManadaCorps © 2024 ",
+                        style: TextStyle(color: Colors.grey[600], fontSize: 14),
+                      ),
+                      Text("Todos los derechos reservados.",
+                        style: TextStyle(color: Colors.grey[600], fontSize: 14),),
+        
+                    ]
+                  )
+                  ,
+                ),
+        
+            ],
           ],
-        ],
+        ),
       ),
     );
 
@@ -203,7 +203,7 @@ class SimonView extends StatelessWidget {
     if(estaEncendido)
       return colorBase;
     else
-      return colorBase.withOpacity(0.3);;
+      return colorBase.withAlpha(76);
   }
   List<BoxShadow> mostrarSombra(bool estaEncendido, Color colorBase){
     if(estaEncendido)
